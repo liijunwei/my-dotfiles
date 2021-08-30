ddcd5615cc214361c79cba737fa3755bddb9c119
xiaolai <lixiaolai@gmail.com>
2021-08-30 21:34:23 +0800

    updated

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@
 rsync $HOME/.aliases .aliases
 rsync $HOME/.functions .functions
 rsync $HOME/.zshrc .zshrc
+rsync $HOME/.zprofile .zprofile
 rsync $HOME/.gitignore_global .gitignore_global
 rsync $HOME/.gitconfig .gitconfig
 rsync $HOME/.jupyter/jupyter_lab_config.py .jupyter/jupyter_lab_config.py
