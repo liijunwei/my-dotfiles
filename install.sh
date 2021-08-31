ded540dc387aad3a1b5c89e692170343283bba39
xiaolai <lixiaolai@gmail.com>
2021-08-31 10:48:11 +0800

    Updated

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/zsh
-
-rsync .aliases $HOME/.aliases
-rsync .functions $HOME/.functions
-rsync .zshrc $HOME/.zshrc
-rsync .zprofile $HOME/.zprofile
-rsync .gitignore_global $HOME/.gitignore_global
-rsync .gitconfig $HOME/.gitconfig
-# rsync .jupyter/jupyter_lab_config.py $HOME/.jupyter/jupyter_lab_config.py
-
+echo 'Download dot-files from https://raw.githubusercontent.com/xiaolai/my-dotfiles/ ...'
+echo
+cd $HOME
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.aliases --output .aliases --silent
+echo '.aliases put into the place'
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.functions --output .functions --silent
+echo '.functions put into the place'
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.zshrc --output .zshrc --silent
+echo '.zshrc put into the place'
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.zprofile --output .zprofile --silent
+echo '.zprofile put into the place'
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.gitignore_global --output .gitignore_global --silent
+echo '.gitignore_global put into the place'
+curl https://raw.githubusercontent.com/xiaolai/my-dotfiles/master/.gitconfig --output .gitconfig --silent
+echo '.gitconfig put into the place'
+echo
 echo 'Installation Completed!'
